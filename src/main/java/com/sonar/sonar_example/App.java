1fd2158ffece29ffb3c356f22517b0ad88fdb767
Nitheesh3009 <nitheeshe2@gmail.com>
2023-08-01 12:36:36 -0500

    SOnar example commit

---
+++ src/main/java/com/sonar/sonar_example/App.java
@@ -0,0 +1,38 @@
+package com.sonar.sonar_example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public App() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("Constructor....");
+	}
+    
+    public void m1() {
+    	String s="";
+    	List<Integer> list = new ArrayList<Integer>();
+    	list.add(1);
+    	list=null;
+    	list.add(2);
+    	Object obj = getData();
+    	System.out.println(obj.toString());
+    }
+    
+    public Object getData() {
+    	return null;
+    }
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        App app = new App();
+        app.m1();
+    }
+}
